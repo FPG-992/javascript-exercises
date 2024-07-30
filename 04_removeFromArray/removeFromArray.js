a5e7989ea8ee164c4f23d05af44680f47e655012
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-07-30 23:45:52 +0300

    exercise 4 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(Array,...args) {
+    return Array.filter((item) => !args.includes(item));   //arrow function, it creates a new array with all the elements
+    // that pass the test implemented by the provided function. item is each element of the array and args is the array of values to remove
+    //if item is not in args, it will be included in the new array
 };
 
 // Do not edit below this line
