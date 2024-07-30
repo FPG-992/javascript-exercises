32792c28ba0bbb781b7b339890fae6b1e12bf1e9
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-07-30 23:53:57 +0300

    exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber,secondNumber) {
+let totalSum = 0;
+if (firstNumber < 0 || secondNumber < 0 || typeof firstNumber !== 'number' || typeof secondNumber !== 'number' 
+    || !Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)
+) {
+    return 'ERROR';
+}
+for (let i = Math.min(firstNumber,secondNumber); i <= Math.max(firstNumber,secondNumber); i++) {
+    totalSum += i;
 };
-
+return totalSum;
+}
 // Do not edit below this line
 module.exports = sumAll;
