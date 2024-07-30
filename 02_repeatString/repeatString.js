6a14e210cbdc4778b956a349f8a25d0c07024842
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-07-30 23:13:41 +0300

    Ex 1 2 3 solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+let stringToRepeat;
+let numberOfTimes;
 
-};
+const repeatString = function(stringToRepeat, numberOfTimes) {
+    let totalString = '';
+    if (numberOfTimes < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < numberOfTimes; i++) {
+        totalString += stringToRepeat;
+    }
+    return totalString;  
+} 
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let ReversedString = '';
+    for (let i=stringToReverse.length-1; i>=0; i--){
+        ReversedString += stringToReverse[i];
+    }
+    return ReversedString;
 };
 
 // Do not edit below this line
