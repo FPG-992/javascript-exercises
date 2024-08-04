2968120b5330c13f9b22ca4f9e335374714af0ea
FPG-992 <78109143+FPG-992@users.noreply.github.com>
2024-08-04 20:04:47 +0300

    Exercise 09 Solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const clearWord = word.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+    let j=clearWord.length-1;
+    for (let i=0; i<word.length; i++){
+        if (clearWord.charAt(i)!==clearWord.charAt(j)){
+            return false;
+        }
+        j--;
+    }
+    return true;
+}
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
