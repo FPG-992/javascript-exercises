6f69a4dbf231ee6fcd8163ee03fd616f9d380237
FPG-992 <78109143+FPG-992@users.noreply.github.com>
2024-08-04 19:22:43 +0300

    Exercise 8 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(number1,number2) {
+  let sum = 0;
+  sum = number1 + number2;
+  return sum;
+}
+
+const subtract = function(number1,number2) {
+  let sum = 0;
+  sum = number1 - number2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+  let sum=0;
+	for (let i =0; i<array.length; i++){
+    sum += array[i];
+  }
+  return sum;
 };
 
-const sum = function() {
-	
+const multiply = function(array) {
+  let multiplyResult = 1;
+  for (let i =0; i<array.length; i++){
+    multiplyResult *= array[i];
+  }
+  return multiplyResult;
 };
 
-const multiply = function() {
+const power = function(number1,number2){
+  return number1**number2;
+}
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  product = 1;
+  for (let i =num; i>0; i--){
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
